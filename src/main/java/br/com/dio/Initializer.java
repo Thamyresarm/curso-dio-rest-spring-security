5c29b6330f8b4041c527cdd212bc3b27fd7b9c83
Thamyresarm <pink.thamyres@gmail.com>
2022-07-29 12:04:09 -0300

    exemplos de aula

---
+++ src/main/java/br/com/dio/Initializer.java
@@ -0,0 +1,42 @@
+package br.com.dio;
+
+import br.com.dio.model.User;
+import br.com.dio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+import java.util.Arrays;
+
+@Component
+public class Initializer implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        User user = repository.findByUsername("admin");
+        if (user == null) {
+            user = new User();
+            user.setName("ADMIN");
+            user.setUsername("admin");
+            user.setPassword("master123");
+            user.getRoles().add("MANAGERS");
+            repository.save(user);
+        }
+        user = repository.findByUsername("user");
+        if (user == null ){
+            user = new User();
+            user.setName("USER");
+            user.setUsername("user");
+            user.setPassword("user123");
+            user.getRoles().add("USERS");
+            repository.save(user);
+        }
+    }
+}
---
+++ src/main/java/br/com/dio/ProjetoRestSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestSpringSecurityApplication.class, args);
+	}
+
+}
